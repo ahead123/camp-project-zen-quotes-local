f1b6fd82b2fa1ae98ed6505a9475d3822778dabe
Arthur Head <arthurallenhead@yahoo.com>
2015-03-23 00:22:32 -0700

    cleaned up main.js

--- src/main.js
+++ src/main.js
@@ -3,15 +3,12 @@
 $(document).ready(function() {
 
 	var $newquote;
-	var quoteArray = [];
 	var $quotelist;
 	var storedQuotes;
 
 	$('button.add-quote').click(function() {
 		// store the value of users input into a variable called userquote
 		$newquote = $('input.new-quote').val();
-		// clicking the add button will push quotes into an array
-		quoteArray.push($newquote);
 		
 		// and prepend quote to the quote list view
 		$quotelist = $('ul.quote-list');
@@ -19,13 +16,19 @@ $(document).ready(function() {
 
 		// clear the input value
 		$('input.new-quote').val("");
-		storedQuotes = $('ul.quote-list').html();
-		localStorage.setItem('storedQuotes', storedQuotes);
+
+		push();
+
 	});
 
 	if(localStorage.getItem('storedQuotes')) {
 			$('ul.quote-list').html(localStorage.getItem('storedQuotes'));
 		}
+
+	function push() {
+		storedQuotes = $('ul.quote-list').html();
+		localStorage.setItem('storedQuotes', storedQuotes);
+	}
 
 	function clear() {
 		window.localStorage.clear();
@@ -35,3 +38,4 @@ $(document).ready(function() {
 	$('button.clear-all-quotes').click(clear);
 	
 });
+
